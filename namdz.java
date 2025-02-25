694ab0a1b65e78a00437f811ff0edb8fa1560fc0
HiNam69 <phamduonghainam@gmail.com>
2025-02-25 10:47:30 +0700

    Create namdz.java
    hehehehehe

---
+++ namdz.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class ictu{
+    public static int Ictu(int x ){
+       int res = 0;
+       for(int i = 1 ; i <= x ; i++){
+        String temp = String.valueOf(i);
+        temp = temp + temp;
+        int num = Integer.parseInt(temp);
+        res += num;
+       }
+       return res;
+    }
+}
+public class solve {
+
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Nhap so N");
+    int x  = scanner.nextInt();
+    System.out.println("S = " + ictu.Ictu(x));
+    scanner.close();
+}
+}
